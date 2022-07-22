1f51a172c48f05e87aa3b1532d46e3dcba1a2c4e
7Yura <svido777@gmail.com>
2022-07-22 08:20:19 +0300

    new folder

---
+++ HOME WORK/JavaScript/hw1/main.js
@@ -0,0 +1,48 @@
+let a = ' hello';
+console.log(a);
+let b = ' owu';
+console.log(b);
+let c = ' com';
+console.log(c);
+let d = ' ua';
+console.log(d);
+let q = 1;
+let w = 10;
+let e = -999;
+let r = 123;
+let t = 3.14;
+let y = 2.7;
+let u = 16;
+let bool1 = true;
+let bool2 = false;
+console.log(q, w, e, r, t, y, u, bool1, bool2);
+alert(a);
+alert(b);
+document.write(a);
+document.write(b, c, d,' ' + q + ' ' + w + ' ' + e + ' ' + r + ' ' + t + ' ' + y + ' ' + u + ' ' + bool1+ ' ' +bool2);
+let firstName = " Yuriy";
+let middleName = ' Mikolayovich';
+let lastName = ' Sviderskiy';
+let person = firstName + middleName + lastName;
+console.log(person);
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+let z = prompt(firstName);
+console.log(z)
+let z2 = prompt(middleName);
+console.log(z2)
+let z3 = prompt(lastName);
+console.log(z3)
+
+
+
+
+
+
+
+
+
