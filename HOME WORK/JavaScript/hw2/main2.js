b86efd000e5523bf1145740b52be0c3ecd6dd5b0
7Yura <svido777@gmail.com>
2022-07-29 16:41:02 +0300

    new folder

--- HOME WORK/JavaScript/hw2/main2.js
+++ HOME WORK/JavaScript/hw2/main2.js
@@ -43,27 +43,27 @@ console.log(book6);
 
 // - Створити масив з 10 об'єктами Шкі описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
 let spisok = [
-    {name: 'Petr', username: 'Petrov', password:10},
-    {name:'Ivan', username: 'Ivanov', password:13},
-    {name:'Nikolay', username: 'Nikolaev', password:56},
-    {name: 'Ivan', username: 'Franko', password:31},
-    {name:'Vasiliy', username: 'Gonta', password:45},
-    {name:'lesia' , username: 'Horush', password:56},
-    {name:'Tatiana' , username: 'Ledneva', password:43},
-    {name: 'Mavr', username: 'Dobrodiy', password:23},
-    {name: 'Svetlana', username: 'Holodna', password:98},
-    {name: 'Anton', username: 'Percheniy', password:76}
+    {name: 'Petr', username: 'Petrov', password: 10},
+    {name: 'Ivan', username: 'Ivanov', password: 13},
+    {name: 'Nikolay', username: 'Nikolaev', password: 56},
+    {name: 'Ivan', username: 'Franko', password: 31},
+    {name: 'Vasiliy', username: 'Gonta', password: 45},
+    {name: 'lesia', username: 'Horush', password: 56},
+    {name: 'Tatiana', username: 'Ledneva', password: 43},
+    {name: 'Mavr', username: 'Dobrodiy', password: 23},
+    {name: 'Svetlana', username: 'Holodna', password: 98},
+    {name: 'Anton', username: 'Percheniy', password: 76}
 ];
 console.log(spisok);
-console.log(spisok[0].username,spisok[0].password);
-console.log(spisok[1].username,spisok[1].password);
-console.log(spisok[2].username,spisok[2].password);
-console.log(spisok[3].username,spisok[3].password);
-console.log(spisok[4].username,spisok[4].password);
-console.log(spisok[5].username,spisok[5].password);
-console.log(spisok[6].username,spisok[6].password);
-console.log(spisok[7].username,spisok[7].password);
-console.log(spisok[8].username,spisok[8].password);
-console.log(spisok[9].username,spisok[9].password);
+console.log(spisok[0].username, spisok[0].password);
+console.log(spisok[1].username, spisok[1].password);
+console.log(spisok[2].username, spisok[2].password);
+console.log(spisok[3].username, spisok[3].password);
+console.log(spisok[4].username, spisok[4].password);
+console.log(spisok[5].username, spisok[5].password);
+console.log(spisok[6].username, spisok[6].password);
+console.log(spisok[7].username, spisok[7].password);
+console.log(spisok[8].username, spisok[8].password);
+console.log(spisok[9].username, spisok[9].password);
 
 
